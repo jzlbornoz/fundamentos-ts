d35de811f8ad6d23ea80a1e3314a2d18103abc34
jzlbornoz <javieraap10@gmail.com>
2023-01-20 22:36:42 -0400

    [add] faker.js

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,54 +1,50 @@
 import { usersList, addUser, calcUsersPoints } from './users/user.services';
 import { ROLES } from './users/user.model';
 import { addProduct, productsList } from './product/product.services';
+import { faker } from '@faker-js/faker';
 
-addUser({
-  id: Math.random(),
-  userName: 'Juan12',
-  createdAt: new Date(),
-  password: '13232',
-  points: 100,
-  role: ROLES.USER,
-  updateAt: new Date(),
-});
-
-addUser({
-  id: Math.random(),
-  userName: 'gonzalo',
-  createdAt: new Date(),
-  password: 12,
-  points: 23,
-  role: ROLES.SELLER,
-  updateAt: new Date(),
-});
-
-addProduct({
-  id: Math.random(),
-  name: 'shoe',
-  createdAt: new Date(),
-  updateAt: new Date(),
-  seller: {
-    id: Math.random(),
-    userName: 'gonzalo',
+for (let index = 0; index < 5; index++) {
+  addUser({
+    id: faker.datatype.uuid(),
+    userName: faker.name.fullName(),
     createdAt: new Date(),
-    password: 12,
-    points: 23,
-    role: ROLES.SELLER,
+    password: faker.internet.password(),
+    points: Math.max(100),
+    role: ROLES.USER,
     updateAt: new Date(),
-  },
-  category: {
-    id: 1,
+  });
+}
+
+for (let i = 0; i < 3; i++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.product(),
     createdAt: new Date(),
     updateAt: new Date(),
-    name: 'sport',
-  },
-  stock: 12,
-  sizes: 'S',
-});
-
+    seller: {
+      id: faker.datatype.uuid(),
+      userName: faker.name.fullName(),
+      createdAt: new Date(),
+      password: faker.internet.password(),
+      points: Math.max(100),
+      role: ROLES.USER,
+      updateAt: new Date(),
+    },
+    category: {
+      id: faker.datatype.uuid(),
+      createdAt: new Date(),
+      updateAt: new Date(),
+      name: faker.commerce.department(),
+    },
+    stock: Math.max(100),
+    sizes: 'S',
+  });
+}
 const res = calcUsersPoints(usersList);
 
 console.log('usersList: ', usersList);
 console.log('Total de puntos: ', res);
 
+console.log('=================================');
 console.log('ProductsList: ', productsList);
+console.log(productsList.length);
